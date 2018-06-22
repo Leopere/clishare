21e991dca60a6bae827140c30cd5ca7efa059a43
chamunks <chamunks@gmail.com>
2018-06-22 19:40:23 -0400

    smarter path restoration

--- tmux-add.sh
+++ tmux-add.sh
@@ -2,10 +2,12 @@
 case $1 in
   install )
   source <(curl -s https://gist.githubusercontent.com/ryin/3106801/raw/c3382b8bda16706f530614329b0af7d0e90075aa/tmux_local_install.sh)
+  echo $PATH > /tmp/original_path
   echo $PATH | grep -q '$HOME/local/bin'; [ $? -ne 0 ] && export PATH=$PATH:'$HOME/local/bin'
     ;;
   uninstall )
     tmux kill-server
     rm -Rf $HOME/local
+    export PATH=$(cat /tmp/original_path)
     ;;
 esac
